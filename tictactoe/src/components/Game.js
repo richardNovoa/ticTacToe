90dddd7835baa37d754f0770c431acf7d8b3a34f
richNovoa <56084025+richardNovoa@users.noreply.github.com>
2021-12-29 09:18:18 -0500

    setWinner

--- tictactoe/src/components/Game.js
+++ tictactoe/src/components/Game.js
@@ -1,10 +1,19 @@
 import Board from "./Board";
 import React from "react";
-
 class Game extends React.Component {
- 
   render() {
-    return <Board />;
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
 }
+
 export default Game;
